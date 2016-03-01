b7d8748b488aa92177daf3ec9d5e59ff04793a00
DecaYale <deca-yale@foxmail.com>
2016-03-01 11:16:53 +0800

    merge sort from top is added

--- Algorithm/Algorithm/Sort.h
+++ Algorithm/Algorithm/Sort.h
@@ -60,8 +60,45 @@ public:
 	void Shell(vector<T> & a)
 	{
 		int N = a.size();
-		
+		//...
 	}
+
+	//πÈ≤¢≈≈–Ú
+	void merge(vector<T> & a ,vector<T> & aux, int lo, int mid, int hi)//[lo..mid] ∫Õ[mid+1..hi]
+	{
+		int i = lo,j =mid+1;
+
+		for (int k=lo;k<=hi;k++)//!!
+		{
+			aux[k] = a[k];
+		}
+		for(int k=lo; k <= hi; k++)
+		{
+			if (i>mid) a[k] = aux[j++];
+			else if (j>hi) a[k] = aux[i++];
+			else if ( less(aux[i],aux[j]) ) a[k] = aux[i++];
+			else a[k] = aux[j++];
+		}
+	}
+	void mSort(vector<T> & a, vector<T> & aux, int lo, int hi)
+	{
+		if (lo >= hi) return;
+		int mid = lo + (hi -lo)/2;//(lo+hi)/2;
+		mSort(a,aux,lo,mid);
+		mSort(a,aux,mid+1,hi);//mSort(a,aux,mid,hi);
+		merge(a,aux,lo,mid,hi);
+
+	}
+	void MergeSort(vector<T>& a )
+	{
+		vector <T> * aux = new vector <T>(a.size());
+		mSort(a,*aux,0,a.size()-1);
+		//for (int i=0;i<aux->size();i++)
+		//	a[i] = (*aux)[i];
+		delete aux;
+	}
+	
+
 
 	void test()
 	{
@@ -70,7 +107,7 @@ public:
 		int input;
 		vector<int> buf;
 		vector<int> buf2;
-
+		vector<int> buf3;
 		//buf.push_back(10);
 		//buf.push_back(4);
 		//buf.push_back(1);
@@ -81,19 +118,26 @@ public:
 		{
 			buf.push_back(input);
 			buf2.push_back(input);
+			buf3.push_back(input);
 		}
 		s.Selection(buf);
 		s.Insertion(buf2);
+		s.MergeSort(buf3);
 		std::cout<<"result of selection sort: "<<'\n';
 		for(int i=0; i < buf.size();i++)
 		{
 			std::cout<<buf.at(i)<<' ';
 		}
 
-		std::cout<<"result of insertion sort: "<<'\n';
+		std::cout<<"\nresult of insertion sort: "<<'\n';
 		for(int i=0; i < buf2.size();i++)
 		{
 			std::cout<<buf2.at(i)<<' ';
+		}
+		std::cout<<"\nresult of merge sort: "<<'\n';
+		for(int i=0; i < buf3.size();i++)
+		{
+			std::cout<<buf3.at(i)<<' ';
 		}
 
 		std::cout<<endl;
